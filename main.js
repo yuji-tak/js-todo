871d95f5faf2cf39d23093a9c20ae015a0fef067
yuji-tak <58871868+yuji-tak@users.noreply.github.com>
2020-05-16 21:59:20 +0900

    削除機能の追加

--- main.js
+++ main.js
@@ -4,115 +4,48 @@ const inputValue = document.getElementById('js_add_value') // 入力元を取得
 const toDoList = []; // 配列で、連想配列を格納
 let id = 0;
 
+const WORKING = '作業中'; // 作業状態のテキスト要素
+const COMPLETED = '完了';
+const DELETE = '削除';
+
 // * todoを表示する関数を定義
-function displayTodo(toDo) {
+function displayTodo(toDo, id, done, trash) {
+    if (trash) { return; } // trashがtrueならここで処理を終了
+    const DONE = done ? COMPLETED : WORKING ; // 三項演算子による条件分岐でテキスト要素を制御
     const text = `<tr>
                     <td>${id}</td>
                     <td>${toDo}</td>
-                    <td><button>作業中</button></td>
-                    <td><button>削除</button></td>
+                    <td><button>${DONE}</button></td>
+                    <td><button>${DELETE}</button></td>
                  </tr>`;
-    
     const position = 'beforeend';
     targetTbody.insertAdjacentHTML(position, text);
 }
-// ** クリックイベントを定義
+// ** todo追加：クリックイベントを定義
 trigerEvent.addEventListener('click', function() {
     const toDo = inputValue.value;
     if (toDo) { // 入力値が入力されていれば関数を実行
-        displayTodo(toDo, id, false, false);
+        displayTodo(toDo, id, false, false); // toDo・idのみ変数
         toDoList.push(
             {
                 id: id,
                 name: toDo,
-                // done: false,
-                // trash: false
+                done: false,
+                trash: false,
             }
         );
     }
     inputValue.value = ''; // イベント終了後、空欄にする
-    id++;
+    id++;    
 });
-
 console.log(toDoList);
 
-// アロー関数
-// var getTriangle = function (base, height) {
-//   return (base * height) / 2;
-// };
-// console.log(getTriangle(2, 4));
-    // ↓書き換え
-// var getTriangle = (base, height) => {
-//   return (base * height) / 2;
-// };
-// console.log(getTriangle(10, 2));
-    // 他パターン
-// const count = n => {
-//     return n * n;
-// }
-// console.log(count(5));
-//     // ↓さらに処理内容のステートメントが1つだけであれば、{}を省略できる。さらに{}省略時はステートメントの結果が返り値になるので、returnが不要になる
-// const count2 = s => s * s;
-// console.log(count2(6));
+// ** todo削除：クリックイベントを定義
+targetTbody.addEventListener('click', e => {
+    e.target.parentNode.parentNode.remove();
+});
 
-// const arr = [1, 5, 7, 3, 4, 6, 13, 10];
-//     // filter()メソッドは、引数として与えられた関数を各配列要素に対して実行し、それに合格したすべての配列要素からなる新しい配列を生成する。
-// const arr1 = arr.filter(function(m) {
-//     return m % 2 == 0;
-// });
-// console.log(arr1);
-//     // ↓書き換え
-// const arr2 = arr.filter(s => s % 2 == 0);
-// console.log(arr2);
 
-// 対象要素.addEventLitener(種類, 関数, false)
-// 第一引数にイベントの種類を指定することで、このイベントがどのようなケースに対応するのかを特定
-// 第二引数に関数を指定することで、任意のイベントが発生した時に関数内に書かれた処理を実行できる
-// 第三引数は、イベントの伝播の方式をtrue/falseで指定する(通常はfalse)
-// const btn = document.getElementById('btn_add');
-// btn.addEventListener('click', function() {
-//     console.log('クリックされたよ');
-// }, false);
-
-// window.addEventListener('keydown', function(event) {
-//     console.log(event.keycode);
-// });
-// window.addEventListener('DOMContentLoaded', function() {
-//     console.log('読まれた');
-// });
-
-// フォーム部品のname属性は「document.forms」から取得できる
-
-// イベントハンドラーの登録
-    // この方法では同一の要素/同一イベントに対して、複数のイベントハンドラーを紐付けられない
-// window.onload = function() {
-//     document.getElementById('btn').onclick = function() {
-//         window.alert('ほ');
-//     }
-// }
-// ページロード時に実行されるイベントリスナーを登録
-// document.addEventListener('DOMContentLoaded', function() {
-//     // クリック時に実行されるイベントリスナーを登録
-//     document.getElementById('btn').addEventListener('click', function() {
-//         window.alert('ほ');
-//     }, false);
-// }, false);
-// 新たに生成されたtrをHTMLへ挿入し、タグ内にtdを挿入
-        // addTarget.insertAdjacentElement('beforeend', trTag);
-        // trTag.insertAdjacentElement('beforeend', tdTagCom);
-    // ①td：ID
-        // const tdTagId = document.createElement('td');
-        // const num = addTarget.childElementCount;
-        //     // データ型を文字列へ変換
-        // const numString = num.toString(10);
-        // tdTagId.innerText = numString;
-        //     // ②td：コメント
-        //     // td要素を生成
-        // const tdTagCom = document.createElement('td');
-        //     // 入力値をテキストノードとして生成
-        // const addValueText = document.createTextNode(addValue.value);
-        //     // tdタグにテキストノード（入力値）を挿入
-        // tdTagCom.appendChild(addValueText);
 
 // // ### バックアップ：05141430：タスク追加機能
 //     // ■ 必要な要素の取得
